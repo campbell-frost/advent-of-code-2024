03d87addf9e8eb3778a2312f1aa36cdd9728537a
campbell-frost <campbellsfrost@gmail.com>
2024-12-01 17:07:20 -0500

    clean up

--- day1/main.ts
+++ day1/main.ts
@@ -1,5 +1,4 @@
 const input: string = await Deno.readTextFile("input.txt");
-// part 1
 const result = input.split("\n")
   .map(line => line.split("   "))
   .reduce<[number[], number[]]>(
@@ -12,5 +11,4 @@ const result = input.split("\n")
     index === 0 ? c : c.map(val => Math.abs(val * a.reduce((a, v) => (v === val ? a + 1 : a), 0)))
   )
   .reduce((a, c) => a + c);
-
 console.log(result);
